8b54288ce19bba0cac88ae0e83564b50be3c2695
Lucivando Sousa <lucivandosousa@gmail.com>
2024-05-13 20:22:25 -0300

    chore: change header color

--- src/components/header/intex.jsx
+++ src/components/header/intex.jsx
@@ -2,7 +2,7 @@ import ToggleTheme from '../toggleTheme'
 
 export default function Header() {
   return (
-    <div className="flex h-20 bg-gray-500 justify-between items-center px-5 sm:rounded-xl sm:m-5">
+    <div className="flex h-20 bg-slate-900 dark:bg-gray-500 justify-between items-center px-5 sm:rounded-xl sm:m-5">
       <span className="text-gray-100">Olá, Usuário</span>
       <h1 className="text-gray-100 text-3xl">Newsletter Tailwind</h1>
       <ToggleTheme />
