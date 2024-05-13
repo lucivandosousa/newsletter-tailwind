50f83b5715e473612b7e6208f6412e2f0a0812c1
Lucivando Sousa <lucivandosousa@gmail.com>
2024-05-13 09:13:09 -0300

    feat: create list of articles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import ArticleList from "./components/articleList";
 import Header from "./components/header/intex";
 
 export default function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <ArticleList />
+    </>
   )
 }
